d4c2d473c1277c20681419c862c2eddec64fdb0a
Mikhail Nepomnyashchiy <mnepomnyashiy@specialist.ru>
2020-09-16 21:13:47 +0300

    posts with class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import ClickerFn from './components/ClickerFn/ClickerFn';
-import ClickerClass from './components/ClickerClass/ClickerClass';
+import PostList from './components/PostList/PostList';
 
 function App() {
   return (
     <div className="App">
-      Hello from React
-      <br />
-      <ClickerFn />
-      <br />
-      <ClickerClass />
+      <PostList />
     </div>
   );
 }
