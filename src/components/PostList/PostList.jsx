9a8661cb0c120320a3c452d614670d77e18739f8
Mikhail Nepomnyashchiy <mnepomnyashiy@specialist.ru>
2020-09-17 18:49:37 +0300

    practice2 complete

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,7 +1,24 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import PostItem from '../PostItem/PostItem';
 
 const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+const PostList = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch(postsUrl)
+            .then(response => response.json())
+            .then(data => setPosts(data))
+    }, []);
+
+    return <div>
+        {posts[0] ? <PostItem 
+            title={posts[0].title}
+            body={posts[0].body}
+            /> : null}
+    </div>
+}
 
 class PostList extends Component {
     state = {
